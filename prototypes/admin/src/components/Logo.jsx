f81ed43dd56c0c0a6bf2eeed6489fd95158fdd4a
Ahmed Albanna <ahmedalbanna966@gmail.com>
2026-05-15 06:47:50 +0200

    add logo files to public

--- prototypes/admin/src/components/Logo.jsx
+++ prototypes/admin/src/components/Logo.jsx
@@ -1,5 +1,5 @@
 export default function Logo({ className = "w-32" }) {
   return (
-    <img src="/logo.svg" alt="عتاد" className={className} />
+    <img src="/Logo-White.svg" alt="عتاد" className={className} />
   );
 }
